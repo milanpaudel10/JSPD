580104a8b67c3532554861b0279a645426f3423e
milanpaudel10 <milanpaudel107@gmail.com>
2026-07-01 20:58:07 +0545

    Day 6

---
+++ JS/Day 6/pop.js
@@ -0,0 +1,6 @@
+let colors = ["Red", "Blue", "Green"];
+
+let removed = colors.pop();
+
+console.log("Removed:", removed);
+console.log("Remaining:", colors);
---
+++ JS/Day 6/push.js
@@ -0,0 +1,5 @@
+let numbers = [1, 2, 3];
+
+numbers.push(4, 5, 6);
+
+console.log(numbers);
