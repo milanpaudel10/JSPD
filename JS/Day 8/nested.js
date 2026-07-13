66418866308f596f2e47dfce207f819d16e60d96
milanpaudel10 <milanpaudel107@gmail.com>
2026-07-13 09:39:23 +0545

    Day 8

---
+++ JS/Day 8/nested.js
@@ -0,0 +1,16 @@
+const player = {
+    name: "Milan",
+    stats: {
+        matches: 25,
+        runs: 1350,
+        average: 54
+    },
+    trophies: [
+        "League Cup",
+        "National Cup"
+    ]
+};
+
+console.log(player.stats.runs);
+console.log(player.trophies[0]);
+console.log(player.stats.average);
---
+++ JS/Day 8/object.js
@@ -0,0 +1,15 @@
+const player = {
+    name: "Milan",
+    age: 20,
+    country: "Nepal",
+    isBatsman: true
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player.age);
+
+player.age = 21;
+player.team = "Nepal";
+
+console.log(player);
