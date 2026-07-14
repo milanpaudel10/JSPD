97179c68d452f5549a24218b9fb57ed68cab29b2
milanpaudel10 <milanpaudel107@gmail.com>
2026-07-14 18:32:00 +0545

    DAY 9

---
+++ JS/Day 9/combine_navigation.js
@@ -0,0 +1,17 @@
+const card = document.querySelector("#card");
+
+console.log(card);
+
+console.log(card.parentElement);
+
+console.log(card.children);
+
+console.log(card.firstElementChild);
+
+console.log(card.lastElementChild);
+
+const heading = card.firstElementChild;
+
+console.log(heading.nextElementSibling);
+
+console.log(heading.parentElement);
---
+++ JS/Day 9/sibling_navigation.js
@@ -0,0 +1,9 @@
+const secondItem = document.querySelector("#second");
+
+console.log(secondItem.previousElementSibling);
+
+console.log(secondItem.nextElementSibling);
+
+console.log(secondItem.previousSibling);
+
+console.log(secondItem.nextSibling);
