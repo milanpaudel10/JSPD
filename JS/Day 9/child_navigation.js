3020baaa032f54a270a4c5399107cf6f23ba9743
milanpaudel10 <milanpaudel107@gmail.com>
2026-07-14 18:29:08 +0545

    Day 9

---
+++ JS/Day 9/child_navigation.js
@@ -0,0 +1,13 @@
+const list = document.querySelector("ul");
+
+console.log(list.children);
+
+console.log(list.children[0]);
+
+console.log(list.children[1]);
+
+console.log(list.children[2]);
+
+console.log(list.firstElementChild);
+
+console.log(list.lastElementChild);
---
+++ JS/Day 9/parent_navigation.js
@@ -0,0 +1,5 @@
+const heading = document.querySelector("h1");
+
+console.log(heading.parentElement);
+
+console.log(heading.parentNode);
