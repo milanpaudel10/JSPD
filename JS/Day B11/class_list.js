1f68ce8290168782738c72e4448ddcaecaf113c9
milanpaudel10 <milanpaudel107@gmail.com>
2026-07-17 14:23:12 +0545

    JS

---
+++ JS/Day B11/class_list.js
@@ -0,0 +1,18 @@
+// Add a class
+
+const box = document.querySelector(".box");
+
+box.classList.add("active");
+
+//Remove a Class
+
+box.classList.remove("active");
+
+//toggle a class
+
+box.classList.toggle("active");
+
+
+// check if class exists 
+
+console.log(box.classList.contains("active"));
