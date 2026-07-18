3aac2647a4ecf814c971e5be946f516e58742fa6
milanpaudel10 <milanpaudel107@gmail.com>
2026-07-18 10:26:32 +0545

    JS

---
+++ JS/Day B12/map.js
@@ -0,0 +1,10 @@
+
+const students = [
+    { name: "Milan", marks: 80 },
+    { name: "Ram", marks: 70 },
+    { name: "Sita", marks: 90 }
+];
+
+const names = students.map(student => student.name);
+
+console.log(names);
